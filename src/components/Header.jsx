65cfc14dba4ff1e4266477db623592b220d610e8
Kishore Kumar <kishorekumar7286@gmail.com>
2024-02-19 22:45:32 +0530

    react

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Header = () =>{
+    return(
+        <div className="bg-gray-700 text-white flex items-center justify-center h-10">
+            <p className="text-2xl font-bold">Header</p>
+        </div>
+    );
+}
+export default Header;
